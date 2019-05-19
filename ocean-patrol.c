17d33b1d0017b3e3fdf28816c840e68bac090cc2
thiagolermen <49798588+thiagolermen@users.noreply.github.com>
2019-05-18 21:03:31 -0300

    random spawn e movimentacao dos obstaculos(BUGADO)

--- ocean-patrol.c
+++ ocean-patrol.c
@@ -15,10 +15,13 @@
 
 #define INIMIGO 1//para identificar o tipo de obstaculo
 #define MERGULHADOR 2//para identificar o tipo de obstaculo
-#define QTDMERGULHADORES 6//quantidade maxima de mergulhadores que aparecerao na tela
-#define QTDSUBINIMIGOS 15//quantidade maxima de inimigos que aparecerao na tela
-#define PORCENTAGEMMERGULHADORES 20//porcentagem maxima da quantidade de mergulhadores(para fazer spawn randomico)
-#define PORCENTAGEMSUBMARINOSINIMIGOS 50//porcentagem maxima da quantidade de submarinos inimigos(para fazer spawn de submarinos inimigos)
+#define QTDOBSTACULOS 7
+#define QTDSUBINIMIGOS 7
+#define QTDMERGULHADORES 7
+#define CAPTURADO 1//se o submarino inimigo foi atingido ou o mergulhador capturado
+#define LIVRE 2//se o submarino inimigo ou o mergulhador esta livre na tela
+#define PORCENTAGEMMERGULHADORES 30//porcentagem maxima da quantidade de mergulhadores(para fazer spawn randomico)
+#define PORCENTAGEMSUBMARINOSINIMIGOS 70//porcentagem maxima da quantidade de submarinos inimigos(para fazer spawn de submarinos inimigos)
 
 #define INICIOXSUBMARINO 5//valor de X do primeiro spawn do jogador
 #define INICIOYSUBMARINO 3//valor de Y do primeiro spawn do jogador
@@ -34,7 +37,7 @@ void gameInterface();//imprime a interface
 void desenhaAgua();//desenha a agua
 
 
-typedef struct submarino{
+typedef struct submarino{//estrutura do jogador
     COORD posicao;//COORD e uma estrutura com X e Y
     int orientacao;//orientacao do submarino(direita = 1 ou esquerda = 0)
     int vidas;//vidas do jogador
@@ -42,6 +45,143 @@ typedef struct submarino{
     int pontuacao;//pontuacao do jogador
     int mergulhadores;//quantidade d emergulhadores capturados
 } SUBMARINO;
+
+typedef struct obstaculo{//estrutura dos obstaculos (submarinos inimgos ou mergulhadores)
+    int tipo;//tipo de cada obstaculo (1: SUBMARINO INIMIGO e 2: MERGULHADOR)
+    COORD posicao;// posicao X e Y dos obstaculos na tela
+    int orientacao;//e a orientacao (1 - direita ou 0 - esquerda) dos obstaculos
+    int status;//status do obstaculo(1 - capturado 2 - livre)
+} SUB_INIMIGOS, MERGULHADORES;
+
+void apagaObstaculo(SUB_INIMIGOS submarinoInimigo[], MERGULHADORES mergulhador[], int indice){
+    if(mergulhador[indice].tipo == MERGULHADOR){
+        textcolor(LIGHTGREEN);
+        if(mergulhador[indice].orientacao == DIREITA){
+            cputsxy(mergulhador[indice].posicao.X, mergulhador[indice].posicao.Y, "    ");
+        }else{
+            if(mergulhador[indice].orientacao == ESQUERDA){
+                cputsxy(mergulhador[indice].posicao.X, mergulhador[indice].posicao.Y, "    ");
+            }
+        }
+    }else{
+        if(submarinoInimigo[indice].tipo == INIMIGO){
+            textcolor(LIGHTGRAY);
+            if(submarinoInimigo[indice].orientacao == DIREITA){
+                cputsxy(submarinoInimigo[indice].posicao.X + 3, submarinoInimigo[indice].posicao.Y, "      ");
+                cputsxy(submarinoInimigo[indice].posicao.X, submarinoInimigo[indice].posicao.Y + 1, "          ");
+            }else{
+                if(submarinoInimigo[indice].orientacao == ESQUERDA){
+                    cputsxy(submarinoInimigo[indice].posicao.X + 1, submarinoInimigo[indice].posicao.Y, "      ");
+                    cputsxy(submarinoInimigo[indice].posicao.X, submarinoInimigo[indice].posicao.Y + 1, "          ");
+                }
+            }
+        }
+    }
+}
+void desenhaObstaculo(SUB_INIMIGOS submarinoInimigo[], MERGULHADORES mergulhador[], int indice){
+    if(mergulhador[indice].tipo == MERGULHADOR){
+        textcolor(LIGHTGREEN);
+        if(mergulhador[indice].orientacao == DIREITA){
+            cputsxy(mergulhador[indice].posicao.X, mergulhador[indice].posicao.Y, ">->o");
+        }else{
+            if(mergulhador[indice].orientacao == ESQUERDA){
+                cputsxy(mergulhador[indice].posicao.X, mergulhador[indice].posicao.Y, "o<-<");
+            }
+        }
+    }else{
+        if(submarinoInimigo[indice].tipo == INIMIGO){
+            textcolor(LIGHTGRAY);
+            if(submarinoInimigo[indice].orientacao == DIREITA){
+                cputsxy(submarinoInimigo[indice].posicao.X + 3, submarinoInimigo[indice].posicao.Y, "_|o|__");
+                cputsxy(submarinoInimigo[indice].posicao.X, submarinoInimigo[indice].posicao.Y + 1, "[+(______)");
+            }else{
+                if(submarinoInimigo[indice].orientacao == ESQUERDA){
+                    cputsxy(submarinoInimigo[indice].posicao.X + 1, submarinoInimigo[indice].posicao.Y, "__|o|_");
+                    cputsxy(submarinoInimigo[indice].posicao.X, submarinoInimigo[indice].posicao.Y + 1, "(______)+]");
+                }
+            }
+        }
+    }
+}
+void moveObstaculo(SUB_INIMIGOS submarinoInimigo[], MERGULHADORES mergulhador[]){
+    int i;
+    for(i=0;i<QTDOBSTACULOS;i++){
+        if(submarinoInimigo[i].tipo == INIMIGO){
+            apagaObstaculo(submarinoInimigo, mergulhador, i);
+            if(submarinoInimigo[i].orientacao == DIREITA){
+                submarinoInimigo[i].posicao.X++;
+                if(submarinoInimigo[i].posicao.X >= COLUNA2)
+                    apagaObstaculo(submarinoInimigo, mergulhador, i);
+                desenhaObstaculo(submarinoInimigo, mergulhador, i);
+                }else{
+                    if(submarinoInimigo[i].orientacao == ESQUERDA){
+                        submarinoInimigo[i].posicao.X--;
+                        if(submarinoInimigo[i].posicao.X <= COLUNA1)
+                            apagaObstaculo(submarinoInimigo, mergulhador, i);
+                        desenhaObstaculo(submarinoInimigo, mergulhador, i);
+                    }//if orientacao submarino inimigo
+                }//else
+        }
+        if(mergulhador[i].tipo == MERGULHADOR){
+            apagaObstaculo(submarinoInimigo, mergulhador, i);
+            if(mergulhador[i].orientacao == DIREITA){
+                mergulhador[i].posicao.X++;
+                if(mergulhador[i].posicao.X <= COLUNA1)
+                    apagaObstaculo(submarinoInimigo, mergulhador, i);
+                desenhaObstaculo(submarinoInimigo, mergulhador, i);
+            }else{
+                if(mergulhador[i].orientacao == ESQUERDA){
+                    mergulhador[i].posicao.X--;
+                    if(mergulhador[i].posicao.X >= COLUNA2)
+                        apagaObstaculo(submarinoInimigo, mergulhador, i);
+                    desenhaObstaculo(submarinoInimigo, mergulhador, i);
+                }//if orientacao mergulhador
+            }//else
+        }//if tipo mergulhador
+    }//for
+}
+void inicializaObstaculos(SUB_INIMIGOS submarinoInimigo[], MERGULHADORES mergulhador[]){//inicializa os obstaculos randomicamente
+    int i;
+    srand(time(0));
+    for(i=0;i<QTDOBSTACULOS;i++){//varre todos os elementos dos vetores mergulhador ou submarinoInimigo
+        if(rand()%100 <= PORCENTAGEMMERGULHADORES){//inicializa mergulhadores
+            if(mergulhador[i].tipo != MERGULHADOR && submarinoInimigo[i].tipo != INIMIGO){
+                mergulhador[i].tipo = MERGULHADOR;
+                if(rand()%2 == 1){//se rand == 1 inicializa mergulhador no lado direito
+                    mergulhador[i].orientacao = ESQUERDA;
+                    mergulhador[i].posicao.X = COLUNA2 - 4;
+                    mergulhador[i].posicao.Y = 5 + i*3;//imprime o mergulhador a cada 3 linhas
+                    desenhaObstaculo(submarinoInimigo, mergulhador, i);
+                }else{
+                    if(rand()%2 == 0){//se rand == 0 inicializa mergulhador no lado esquerdo
+                        mergulhador[i].orientacao = DIREITA;
+                        mergulhador[i].posicao.X = COLUNA1 + 1;
+                        mergulhador[i].posicao.Y = 5 + i*3;
+                        desenhaObstaculo(submarinoInimigo, mergulhador, i);
+                    }
+                }
+            }
+        }
+        if(rand()%100 > PORCENTAGEMMERGULHADORES){//inicializa submarinos inimigos
+            if(submarinoInimigo[i].tipo != INIMIGO && mergulhador[i].tipo != MERGULHADOR){
+                submarinoInimigo[i].tipo = INIMIGO;
+                if(rand()%2 == 1){//se rand == 1 inicializa submarino inimigo no lado direito
+                    submarinoInimigo[i].orientacao = ESQUERDA;
+                    submarinoInimigo[i].posicao.X = COLUNA2 - 10;
+                    submarinoInimigo[i].posicao.Y = 5 + i*3;
+                    desenhaObstaculo(submarinoInimigo, mergulhador, i);
+                }else{
+                    if(rand()%2 == 0){
+                        submarinoInimigo[i].orientacao = DIREITA;
+                        submarinoInimigo[i].posicao.X = COLUNA1 + 1;
+                        submarinoInimigo[i].posicao.Y = 5 + i*3;
+                        desenhaObstaculo(submarinoInimigo, mergulhador, i);
+                    }
+                }
+            }
+        }
+    }
+}
 void apagaSubmarino(SUBMARINO jogador){//funcao para apagar o rastro do jogador
     if(jogador.orientacao == DIREITA){
         cputsxy(jogador.posicao.X + 3, jogador.posicao.Y, "        ");
@@ -63,7 +203,7 @@ void desenhaSubmarino(SUBMARINO jogador){//funcao para desenhar o jogador
         cputsxy(jogador.posicao.X, jogador.posicao.Y + 1, "(________)+]");
     }
 }
-void moveJogador(SUBMARINO *jogador){//funcao para mover jogador(submarino)
+void moveJogador(SUBMARINO *jogador, char *flag_fim){//funcao para mover jogador(submarino)
     char tecla;
     if(kbhit()){//vevrifica se o jogador apertou alguma tecla
         tecla = getch();
@@ -117,18 +257,24 @@ void moveJogador(SUBMARINO *jogador){//funcao para mover jogador(submarino)
                 desenhaSubmarino(*jogador);
             }//switch
         }//if
+        if(tecla == 27)
+            *flag_fim = 27;
     }//KBHIT
 }
 void gameLoop(){//laco do jogo
-    char tecla;
+    char tecla;//flag para fim do jogo
+    int contador = 0;
     SUBMARINO jogador;//estrutura do submarino
+    SUB_INIMIGOS submarinoInimigo[QTDSUBINIMIGOS];
+    MERGULHADORES mergulhador[QTDMERGULHADORES];
     jogador.posicao.X = INICIOXSUBMARINO;//coordenadas para inicializar o submarino
     jogador.posicao.Y = INICIOYSUBMARINO;
     jogador.orientacao = DIREITA;//o submarino e inicializado voltado para a direira
     desenhaSubmarino(jogador);
     do{
-        //tecla = getch();
-        moveJogador(&jogador);//chama attraves de ponteiro pos alterara os valores da posicao do submarino
+        moveJogador(&jogador, &tecla);//chama attraves de ponteiro pos alterara os valores da posicao do submarino
+        inicializaObstaculos(submarinoInimigo, mergulhador);
+        moveObstaculo(submarinoInimigo, mergulhador);
     }while(tecla != 27);//jogo roda enquanto o jogador nao teclou ESC
 }
 
@@ -136,30 +282,30 @@ void gameLoop(){//laco do jogo
 
 int main(){
     menu();
+    return 0;
 }
 void menu(){
     char select;//guarda o valor do teclado para mover o seletor
     int posSX = 54, posSY = 6;//posicao inicial do seletor. comeca em 'novo jogo'
-    int ansMenu = 0;//flag para fim do loop do 'menu'
-    clrscr();
-    textcolor(LIGHTCYAN);
-    cputsxy(5, 2,"     ___     ___      __      __       ___");
-    cputsxy(5, 3,"   / __`\\  /'___\\  /'__`\\  /'__`\\   /' _ `\\");
-    cputsxy(5, 4,"  /\\ \\L\\ \\/\\ \\__/ /\\  __/ /\\ \\L\\.\\_ /\\ \\/\\ \\");
-    cputsxy(5, 5,"  \\ \\____/\\ \\____\\\\ \\____\\\\ \\__/.\\_\\\\ \\_\\ \\_\\");
-    cputsxy(5, 6,"   \\/___/  \\/____/ \\/____/ \\/__/\\/_/ \\/_/\\/_/");
-    cputsxy(5, 7,"                  __                 ___");
-    cputsxy(5, 8,"                 /\\ \\__             /\\_ \\");
-    cputsxy(5, 9,"  _____      __  \\ \\ ,_\\  _ __   ___\\//\\ \\");
-    cputsxy(5, 10, " /\\ '__`\\  /'__`\\ \\ \\ \\/ /\\`'__\\/ __`\\\\ \\ \\");
-    cputsxy(5, 11," \\ \\ \\L\\ \\/\\ \\L\\.\\_\\ \\ \\_\\ \\ \\//\\ \\L\\ \\\\_\\ \\_");
-    cputsxy(5, 12,"  \\ \\ ,__/\\ \\__/.\\_\\\\ \\__\\\\ \\_\\\\ \\____//\\____\\");
-    cputsxy(5, 13,"   \\ \\ \\/  \\/__/\\/_/ \\/__/ \\/_/ \\/___/ \\/____/");
-    cputsxy(5, 14,"    \\ \\_\\");
-    cputsxy(5, 15,"     \\/_/");
-    imprimeBordaMenu();
-    putchxy(posSX, posSY, '>');
+    int ansMenu;//flag para fim do loop do 'menu'
     do{
+        textcolor(LIGHTCYAN);
+        cputsxy(5, 2,"     ___     ___      __      __       ___");
+        cputsxy(5, 3,"   / __`\\  /'___\\  /'__`\\  /'__`\\   /' _ `\\");
+        cputsxy(5, 4,"  /\\ \\L\\ \\/\\ \\__/ /\\  __/ /\\ \\L\\.\\_ /\\ \\/\\ \\");
+        cputsxy(5, 5,"  \\ \\____/\\ \\____\\\\ \\____\\\\ \\__/.\\_\\\\ \\_\\ \\_\\");
+        cputsxy(5, 6,"   \\/___/  \\/____/ \\/____/ \\/__/\\/_/ \\/_/\\/_/");
+        cputsxy(5, 7,"                  __                 ___");
+        cputsxy(5, 8,"                 /\\ \\__             /\\_ \\");
+        cputsxy(5, 9,"  _____      __  \\ \\ ,_\\  _ __   ___\\//\\ \\");
+        cputsxy(5, 10, " /\\ '__`\\  /'__`\\ \\ \\ \\/ /\\`'__\\/ __`\\\\ \\ \\");
+        cputsxy(5, 11," \\ \\ \\L\\ \\/\\ \\L\\.\\_\\ \\ \\_\\ \\ \\//\\ \\L\\ \\\\_\\ \\_");
+        cputsxy(5, 12,"  \\ \\ ,__/\\ \\__/.\\_\\\\ \\__\\\\ \\_\\\\ \\____//\\____\\");
+        cputsxy(5, 13,"   \\ \\ \\/  \\/__/\\/_/ \\/__/ \\/_/ \\/___/ \\/____/");
+        cputsxy(5, 14,"    \\ \\_\\");
+        cputsxy(5, 15,"     \\/_/");
+        putchxy(posSX, posSY, '>');
+        imprimeBordaMenu();
         coloreLinhaMenu(&posSY);
         moveSeletor(&select, &posSX, &posSY);
         if(select == 13){//verifica se o usuario apertou enter
@@ -168,6 +314,7 @@ void menu(){
                 clrscr();
                 gameInterface();
                 gameLoop();
+                clrscr();
                 break;
             case(7)://carregar jogo
                 clrscr();
@@ -179,6 +326,7 @@ void menu(){
                 clrscr();
                 break;
             case(10)://sair
+                clrscr();
                 ansMenu = 1;
             }
         }
@@ -217,9 +365,9 @@ void moveSeletor(char *select, int *posSX, int *posSY){
             }else{
                 (*posSY)++;
                 putchxy(*posSX, *posSY, '>');
-            }
-        }
-    }
+            }//else
+        }//switch
+    }//if select
 }
 void coloreLinhaMenu(int *posSY){
     switch(*posSY){//verifica em qual posicao o seletor esta
